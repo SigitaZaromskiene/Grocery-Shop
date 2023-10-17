6e6adf4009ffa4a93fcd0822fbca19e31c289c7a
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-10-17 11:43:44 +0300

    nav titles/logo

--- shop/src/components/Nav.jsx
+++ shop/src/components/Nav.jsx
@@ -1,14 +1,28 @@
-import logo from "../UI/Img/logo.png"
-
+import logo from "../UI/Img/logo.png";
 
 function Nav() {
   return (
     <nav className="nav-container">
       <div className="nav-container_left">
-        <img src={logo} alt='Shop logo leaf'/>
-        <p className="nav-container_left-title">Nature<span>Fresh</span></p>
+        <img src={logo} alt="Shop logo leaf" />
+        <p className="nav-container_left-title">
+          Nature<span>Fresh</span>
+        </p>
       </div>
-      <div className="nav-container-light"></div>
+      <div className="nav-container_middle">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Products</li>
+          <li>Shop</li>
+        </ul>
+      </div>
+      <div className="nav-container_right">
+        <ul>
+          <li>Login</li>
+          <li>Register</li>
+        </ul>
+      </div>
     </nav>
   );
 }
