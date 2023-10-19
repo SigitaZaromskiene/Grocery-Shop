0a9644ceb747d1133ceaf7e390ac089aef76c3f4
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-10-19 17:45:06 +0300

    sign in form

--- shop/src/components/Pages/Login.jsx
+++ shop/src/components/Pages/Login.jsx
@@ -1,17 +1,20 @@
 import Btn from "../Buttons/Btn";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
+import LongBtn from "../Buttons/LongBtn";
+import { NavLink } from "react-router-dom";
 
 function Login () {
  
           return (
+         
             <div className="login_container">
               <form className="login_container_form">
               <FontAwesomeIcon className='icon_leave' icon={faX} />
-                <div className="category_container_heading">
-                  <div className="category_container_border"></div>
-                  <h2>Login Details</h2>
-                  <div className="category_container_border"></div>
+                <div className="login_container_heading">
+                  
+                  <h2>Sign In</h2>
+               
                 </div>
                 <div className="login_container_inputs">
                 <div>
@@ -21,9 +24,15 @@ function Login () {
                   <input className="login_container_input" placeholder="Password"></input>
                 </div>
                 </div>
+                <LongBtn text="Sign In" />
+                <div className="login_container_input_register" >
+                <p>Don't have an account?</p>
+                <NavLink className="login_container_input_register_now" to={'/register'}>SIGN UP NOW</NavLink>
+                </div>
                 </form>
-              <Btn text="Login" />
+        
               </div>
+           
           );
         }
         
