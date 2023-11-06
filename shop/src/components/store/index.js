63af958a5eb453ccf636b8c4d2fcd123e2978cdf
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-11-06 13:40:04 +0200

    fix

--- shop/src/components/store/index.js
+++ shop/src/components/store/index.js
@@ -1,18 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit"
-import uiSlice from "./slices/uiSlice"
-import cartSlice from "./slices/cartSlice"
-import filterSlice from "./slices/filterSlice"
-
-
-
- 
+import { configureStore } from "@reduxjs/toolkit";
+import uiSlice from "./slices/uiSlice";
+import cartSlice from "./slices/cartSlice";
+import filterSlice from "./slices/filterSlice";
 
 const store = configureStore({
-    reducer:
-    { cart: cartSlice.reducer, ui: uiSlice.reducer, filter:filterSlice.reducer } 
-})
+  reducer: {
+    cart: cartSlice.reducer,
+    ui: uiSlice.reducer,
+    filter: filterSlice.reducer,
+  },
+});
 
-export default store
-
-
-
+export default store;
