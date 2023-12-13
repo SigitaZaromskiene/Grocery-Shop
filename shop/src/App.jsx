b9f7b6cb8cc58ebbf5d925c0164b39733b9cc85d
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-12-13 10:29:38 +0200

    git deployment

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -10,27 +10,27 @@ import { useDispatch } from "react-redux";
 import MobileNavigation from "./components/Sections/MobileNavigation";
 
 function App() {
-  const showNotification = useSelector((state) => state.ui.notification);
+  // const showNotification = useSelector((state) => state.ui.notification);
   const showCart = useSelector((state) => state.ui.isCartVisible);
   const navigationBar = useSelector((state) => state.ui.mobileMenuVisible);
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(onPageLoad());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(onPageLoad());
+  // }, [dispatch]);
 
   return (
     <div className="app_container">
       {showCart && <Cart />}
       {navigationBar ? <MobileNavigation /> : <Nav />}
-      {showNotification && (
+      {/* {showNotification && (
         <Notification
           title={showNotification.title}
           text={showNotification.message}
           className={showNotification.status}
         />
-      )}
+      )} */}
       <PageRoutes />
     </div>
   );
